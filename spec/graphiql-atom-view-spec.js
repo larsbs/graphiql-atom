2d338d91f743407d3a1f46b4e88a2ed9a2e8358c
Lorenzo Ruiz <lars.bs@hotmail.com>
2018-02-22 09:59:49 +0100

    Initial commit

---
+++ spec/graphiql-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GraphiqlAtomView from '../lib/graphiql-atom-view';
+
+describe('GraphiqlAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
